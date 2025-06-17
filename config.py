3ef5847d0b502a352e59d94ac69443b5e573b867
ScumDiscovery <scumdiscovery@gmail.com>
2025-06-17 06:41:46 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+WEBHOOK_URL = "https://discord.com/api/webhooks/1383407890663997450/hr2zvr2PjO20IDLIk5nZd8juZDxG9kYkOOZ0c2_sqzGtuXra8Dz-HbhtnhtF3Yb0Hsgi"
+MAP_PATH = "assets/map.png"
+BASE_TEMPLATE_PATH = "templates/base.png"
+WEAPON_ICONS_PATH = "assets/weapons/"
